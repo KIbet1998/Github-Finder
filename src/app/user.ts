02264cf1538584382ce6adbcc94af7e6c178af5f
Kibet 1998 <ronoronald1998@gmail.com>
2021-05-23 17:41:28 +0300

    class updated

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,9 @@
 export class User {
+    constructor(
+        public bio:string,
+        public public_repos:number,
+        public login:string,
+        public avatar_url:String,
+        public created_at:Date
+    ){}
 }
