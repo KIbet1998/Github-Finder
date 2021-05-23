cfb86e2af76a213e9fe6b7ab3779bcbc66ec56ed
Kibet 1998 <ronoronald1998@gmail.com>
2021-05-23 13:23:43 +0300

    add github-form and github-results

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubFormComponent } from './github-form/github-form.component';
+import { GithubResultsComponent } from './github-results/github-results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'github-form', component: GithubFormComponent },
+  {path: 'github-results', component: GithubResultsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
